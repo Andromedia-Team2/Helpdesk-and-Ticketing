142d9b78fca8a5dd606e8cce7bfee30978ce924d
Nadyasal <19390100001@dinamika.ac.id>
2021-10-27 14:14:27 +0700

    Sprint 3

--- app/web/src/components/tabelticket.tsx
+++ app/web/src/components/tabelticket.tsx
@@ -9,18 +9,25 @@ require("datatables.net")(window, $);
 declare const window: BaseWindow;
 
 export default () => {
+  var no = 0;
   api("/api/get-ticket").then((res) => {
     $("#dtable").DataTable({
       data: res.data,
       aoColumns: [
         { 
-          mData: "id_ticket" 
+          mData: null,
+          mRender: function (data, type, full) {
+            return (no + 1);
+          } 
         },
         {
           mData: "judul_ticket",
         },
         {
-          mData: "jenis_ticket",
+          mData: null,
+          mRender: function (data, type, full) {
+            return (full.jenis_ticket.jenis_ticket);
+          }
         },
          {
           mData: "tanggal_masuk",
@@ -29,44 +36,22 @@ export default () => {
           mData: "tanggal_solved",
         },
         {
-          mData: null,
-          bSortable: false,
-          sWidth: "10%",
-          sClass: "center",
-          onclick :"false",
+          "mData": null,
+          "bSortable": false,
+          "sClass": "center",
+          "sWidth": "15%",
           mRender: function (data, type, full) {
-            return (
-              
-              "<button class='space-x-3 items-center justify-center px-2 my-2 py-1 bg-green-500 border  rounded-lg'><a class='text-sm font-sm text-white' " +
-              full.id_status_ticket +
-              "'> OPEN" +
-              
-              "<path fill-rule='evenodd' d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z' clip-rule='evenodd'></path>" +
-              "</svg></a></button></div>"
-
-              // "<button class='space-x-3 items-center justify-center px-2 my-2 py-1 bg-yellow-600 border  rounded-lg'><a class='text-sm font-sm text-white'" +
-              // full.id_status_ticket +
-              // "'> ASSIGNED" +
-              
-              // "<path fill-rule='evenodd' d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z' clip-rule='evenodd'></path>" +
-              // "</svg></a></button></div>"
-
-              // "<button class='space-x-3 items-center justify-center px-2 my-2 py-1 bg-red-600 border  rounded-lg'><a class='text-sm font-sm text-white' " +
-              // full.id_status_ticket +
-              // "'> IN PROGRESS" +
-              
-              // "<path fill-rule='evenodd' d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z' clip-rule='evenodd'></path>" +
-              // "</svg></a></button></div>"
-
-              // "<button class='space-x-3 items-center justify-center px-2 my-2 py-1 bg-gray-800 border  rounded-lg'><a class='text-sm font-sm text-white' " +
-              // full.id_status_ticket +
-              // "'> RESOLVED" +
-              
-              // "<path fill-rule='evenodd' d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z' clip-rule='evenodd'></path>" +
-              // "</svg></a></button></div>"
-            ); 
+            if (full.status_ticket.status_ticket == "OPEN") {
+              return "<div class='flex items-center justify-center text-xs mx-4 my-4 py-2 bg-green-500 text-white rounded-lg'><b>" + full.status_ticket.status_ticket + "</b></div>";
+            } else if (full.status_ticket.status_ticket == "ASSIGNED") {
+              return "<div class='flex items-center justify-center text-xs mx-4 my-4 py-2 bg-yellow-500 text-white rounded-lg'><b>" + full.status_ticket.status_ticket + "</b></div>";
+            } else if (full.status_ticket.status_ticket == "IN PROGRESS") {
+              return "<div class='flex items-center justify-center text-xs mx-4 my-4 py-2 bg-red-500 text-white rounded-xl'><b>" + full.status_ticket.status_ticket + "</b></div>";
+            } else {
+              return "<div class='flex items-center justify-center text-xs mx-4 my-4 py-2 bg-gray-800 text-white rounded-lg'><b>" + full.status_ticket.status_ticket + "</b></div>";
+            }
+          }, 
         },
-      },
       ],
     });
   });
@@ -77,7 +62,7 @@ export default () => {
         <thead>
           <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
             <th className="py-3">No</th>
-            <th className="pl-28 pl-10 py-3">Subject</th>
+            <th className="py-3">Subject</th>
             <th className="py-3">Ticket Type</th>
             <th className="py-3">Create At</th>
             <th className="py-3">Finished At</th>
